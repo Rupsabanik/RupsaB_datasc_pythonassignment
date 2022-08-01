d2c66b25be2d6a1516a66b14682a0f052306de3e
Rupsabanik <110343749+Rupsabanik@users.noreply.github.com>
2022-08-01 09:29:46 +0530

    Update realimg.py

--- Assignment2/realimg.py
+++ Assignment2/realimg.py
@@ -1,3 +1,10 @@
-n=complex(input("Enter complex number: "))
+a=int(input("Enter 1st no:"))
+b=int(input("Enter 2nd no:"))
+n=complex(a,b)
+print("The complex number:",n)
 print("The real part: ",n.real)
 print("The imaginary part: ",n.imag)
+if n.real>n.imag:
+    print(n.real,"is greater")
+else:
+    print(n.imag,"is greater")
