7cd7b42bf3206bf7d5ba8c87a8112e382c0214a8
Rupsabanik <110343749+Rupsabanik@users.noreply.github.com>
2022-08-01 10:16:06 +0530

    Update grade.py

--- Assignment2/grade.py
+++ Assignment2/grade.py
@@ -3,22 +3,14 @@ sub2=int(input("Enter 2nd Subject number: "))
 sub3=int(input("Enter 3rd Subject number: "))
 sub4=int(input("Enter 4th Subject number: "))
 sub5=int(input("Enter 5th Subject number: "))
-sub=sub1 + sub2 + sub3 + sub4 + sub5
-per=sub/5
-if (sub1 and sub2 and sub3 and sub4 and sub5)<=100:
-    if sub>100:
-        print("Result: pass")
-        if per>=90:
-            print("Grade: A+")
-        elif per>=80:
-            print(" Grade: A")
-        elif per>=60:
-            print("Grade: B+")
-        elif per>=40:
-            print("Grade: B")
-        elif per>=20:
-            print("Grade: c")
-    else:
-        print("Result: fail")
+Total=(sub1 + sub2 + sub3 + sub4 + sub5)
+avg=Total/5
+per=(Total/500)*100
+if Total<500:
+    print("1st division\nResult:pass\npercentage=",per)
+elif Total<=420:
+    print("2nd division\nResult:pass\npercentage=",per)
+elif Total<=250:
+    print("3rd division\nResult:pass\npercentage=",per)
 else:
-    print("subject number cannot greater than 100")
+    print("Result: fail")
