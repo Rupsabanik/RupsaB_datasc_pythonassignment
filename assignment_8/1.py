e82d9e26c282f1eb846325510ce6b4c8f6ab3c41
Rupsabanik <110343749+Rupsabanik@users.noreply.github.com>
2022-09-24 00:51:57 +0530

    Add files via upload

---
+++ assignment_8/1.py
@@ -0,0 +1,8 @@
+def average_tuple(nums):
+    result = [sum(x) / len(x) for x in zip(*nums)]
+    return result
+nums = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4))
+print("Original Tuple: ")
+print(nums)
+print("\nAverage value of the numbers of the said tuple of tuples:\n",
+      average_tuple(nums))
